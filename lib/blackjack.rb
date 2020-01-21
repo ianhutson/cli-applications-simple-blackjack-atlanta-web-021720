5e5908ef9ca164d114761e536b8272d922a26844
Ian Hutson <ih00209@georgiasouthern.edu>
2020-01-21 17:16:23 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -46,10 +46,15 @@ def hit?
   user_input = get_user_input
 end
 
-
+if user_input == "h"
+    card_total += deal_card
+  end
+  card_total
+end
 
 def invalid_command
   # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
@@ -58,5 +63,11 @@ end
 
 def runner
   # code runner here
+  welcome
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
 end
-    
+  end_game(card_total)
+end
